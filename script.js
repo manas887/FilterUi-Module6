94e27b1778918a86075662890b47234c314da812
manas887 <116465782+manas887@users.noreply.github.com>
2022-12-10 23:33:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let selectedValue = document.getElementById("list").value;
+function getSelectedValue() {
+    selectedValue = document.getElementById("list").value;
+}
+
+const employeeArr = [
+    { id: 1, name: "John", profession: "Developer", age: "19" },
+    { id: 2, name: "Jack", profession: "Developer", age: "20" },
+    { id: 3, name: "Karen", profession: "Admin", age: "19", },
+];
+
+filterEmployee = () => {
+    var filteredArray = employeeArr.filter(filteredEmployee);
+    var text = "";
+    filteredArray.forEach(myFun);
+
+    function myFun(user) {
+        text +=
+            "<br>" +
+            user.id +
+            "&nbsp&nbsp&nbsp&nbsp&nbsp" +
+            user.name +
+            "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" +
+            user.profession +
+            "&nbsp&nbsp&nbsp&nbsp&nbsp" +
+            user.age +
+            "&nbsp&nbsp&nbsp&nbsp&nbsp"
+            + "<br><br>";
+    }
+    document.getElementById("item").innerHTML = text;
+};
+
+filteredEmployee = (employee) => {
+    if (selectedValue == "Profession") {
+        return true;
+    }
+    return employee.profession == selectedValue;
+};
+filterEmployee();
